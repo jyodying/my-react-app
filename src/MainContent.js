ac991590bb53f4690ee9c9dc35fc9f4464d90dc9
jyodying <xpanderz@yahoo.com>
2019-04-13 10:57:36 -0400

    Todo App - Phase 2

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,20 +1,16 @@
 import React from 'react';
+import TodoItem from 'TodoItem'
 
 function MainContent() {
     return (
         <main>
             <p>This where most of my content will go&hellip;</p>
-            <input type="checkbox" />
-            <p></p>
-
-            <input type="checkbox" />
-            <p></p>
-
-            <input type="checkbox" />
-            <p></p>
-
-            <input type="checkbox" />
-            <p></p>
+            <div className="todo-list">
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div>
         </main>
     )
 
